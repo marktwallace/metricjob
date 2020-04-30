84a45a77a0b3dbb81c16e35e277007bcdbd04a1b
Mark Wallace <markw@Marks-MBP.lan>
2020-04-29 19:47:24 -0700

    Added env support for GaugeVectors

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,6 +3,7 @@ package app
 import (
 	"fmt"
 	"log"
+	"math"
 	"math/rand"
 	"strconv"
 	"time"
@@ -24,11 +25,20 @@ type AppContext struct {
 	CounterVecLabel       []string  `env:"COUNTER_VEC_LABEL"`
 	CounterVecCardinality []int     `env:"COUNTER_VEC_CARDINALITY"`
 	CounterVecRatio       []float64 `env:"COUNTER_VEC_RATIO"`
-	//MetricRanges          []float64 `env:"METRIC_RANGES"` // Integer or float scalar
-	//MetricFuncs           []string  `env:"METRIC_FUNCS"`  // R - Random, S - Sine wave
+
+	GaugeVecName              []string  `env:"GAUGE_VEC_NAME"`
+	GaugeVecLabel             []string  `env:"GAUGE_VEC_LABEL"`
+	GaugeVecCardinality       []int     `env:"GAUGE_VEC_CARDINALITY"`
+	GaugeVecMin               []float64 `env:"GAUGE_VEC_MIN"`
+	GaugeVecMax               []float64 `env:"GAUGE_VEC_MAX"`
+	GaugeVecNoise             []float64 `env:"GAUGE_VEC_NOISE"`
+	GaugeVecPeriodSeconds     []int     `env:"GAUGE_VEC_PERIOD_SECONDS"`
+	GaugeVecPhaseShiftSeconds []int     `env:"GAUGE_VEC_PHASE_SHIFT_SECONDS"`
+
 	// internal state
 	MapToPrometheusCounter    map[string]prometheus.Counter
 	MapToPrometheusCounterVec map[string]*prometheus.CounterVec
+	MapToPrometheusGaugeVec   map[string]*prometheus.GaugeVec
 }
 
 // NewApp create context
@@ -40,6 +50,7 @@ func NewApp() *AppContext {
 
 	app.MapToPrometheusCounter = make(map[string]prometheus.Counter)
 	app.MapToPrometheusCounterVec = make(map[string]*prometheus.CounterVec)
+	app.MapToPrometheusGaugeVec = make(map[string]*prometheus.GaugeVec)
 
 	return app
 }
@@ -61,6 +72,16 @@ func (app *AppContext) GetPrometheusCounterVec(name string, label string) *prome
 			promauto.NewCounterVec(prometheus.CounterOpts{Name: name}, []string{label})
 	}
 	return app.MapToPrometheusCounterVec[key]
+}
+
+func (app *AppContext) GetPrometheusGaugeVec(name string, label string) *prometheus.GaugeVec {
+	key := name + label
+	_, exists := app.MapToPrometheusGaugeVec[key]
+	if !exists {
+		app.MapToPrometheusGaugeVec[key] =
+			promauto.NewGaugeVec(prometheus.GaugeOpts{Name: name}, []string{label})
+	}
+	return app.MapToPrometheusGaugeVec[key]
 }
 
 func (app *AppContext) updateCounters() {
@@ -90,9 +111,32 @@ func (app *AppContext) updateCounterVecs() {
 	}
 }
 
-func (app *AppContext) updateMetrics() {
+func (app *AppContext) updateGaugeVecs(t time.Time) {
+	if len(app.GaugeVecName) == 0 {
+		return
+	}
+	for i, name := range app.GaugeVecName {
+		label := app.GaugeVecLabel[i]
+		vec := app.GetPrometheusGaugeVec(name, label)
+		for k := 0; k < app.GaugeVecCardinality[i]; k++ {
+			period := app.GaugeVecPeriodSeconds[i]
+			shift := k * app.GaugeVecPhaseShiftSeconds[i]
+			phaseSec := (t.Unix() + int64(shift)) % int64(period)
+			phase := (2 * math.Pi * float64(phaseSec)) / float64(period)
+			amplitude := (app.GaugeVecMax[i] - app.GaugeVecMin[i]) / 2.0
+			mean := (app.GaugeVecMax[i] + app.GaugeVecMin[i]) / 2.0
+			val := math.Sin(phase)*amplitude + mean
+			val += rand.Float64() * app.GaugeVecNoise[i]
+			vec.With(prometheus.Labels{label: strconv.Itoa(k)}).Set(val)
+			//fmt.Printf("%d %6.4f %6.2f %s \n", k, phase, val, strings.Repeat("*", int(val)))
+		}
+	}
+}
+
+func (app *AppContext) updateMetrics(t time.Time) {
 	app.updateCounters()
 	app.updateCounterVecs()
+	app.updateGaugeVecs(t)
 }
 
 func (app *AppContext) tickerLoop(ticker *time.Ticker, done chan bool) {
@@ -103,7 +147,7 @@ func (app *AppContext) tickerLoop(ticker *time.Ticker, done chan bool) {
 			return
 		case t := <-ticker.C:
 			metrics.MainLoopCounter.Inc()
-			app.updateMetrics()
+			app.updateMetrics(t)
 			fmt.Println("Tick at", t)
 		}
 	}
